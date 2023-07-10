0510c56f10286b500592751e4261b8144a3ec20f
Toshturobov <toshturobovm@gmail.com>
2023-07-10 17:48:02 +0300

    llyuoy narsa

--- congfig/settings.py
+++ congfig/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(k950pu*ax#nytfaw^8dsv!=@=syl+-usre^n5ewmf&s9+7vt7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("""
+    <style>
+        h1{color:blue;}
+        body{display:flex;justify-content:center;background-color:red;}
+    </style>
+
+    <h1>Home</h1>
+""") 
+
+def about(request):
+    return HttpResponse("""
+    <style>
+        h1{color:blue;}
+        body{display:flex;justify-content:center;background-color:yellow;}
+    </style>
+
+    <h1>About</h1>
+""") 
