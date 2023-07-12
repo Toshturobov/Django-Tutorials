992c9e29ff70b10a6e4a035ad26e1b697e8787db
Toshturobov <toshturobovm@gmail.com>
2023-07-12 17:10:27 +0300

    uy ishi

--- congfig/urls.py
+++ congfig/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("hello_world.urls")),
+    # path("", include("hello_world.urls")),
     path("", include("app_name.urls")),
 ]
